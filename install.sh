4ec3282aaf76f80b2ef5531657030d77742f12f1
Carson <jackparadise520a@gmail.com>
2024-12-13 13:30:15 +0800

    改用yad來發送訊息 zenity在近期mesa更新，會遇到MESA: error: zink: colud not create swapchain的錯誤

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 install_on_chrome() {
   cp manifest_chrome.json manifest.json
   google-chrome chrome://extensions/
-  zenity --info --text="ctrl+L and input: chrome://extensions/"
-  zenity --info --text="指派vimium的目錄給它即可"
+  yad --info --text="ctrl+L and input: chrome://extensions/"
+  yad --info --text="指派vimium的目錄給它即可"
 }
 
 install_on_filefox() {
   cp manifest_firefox.json manifest.json
   firefox about:debugging#/runtime/this-firefox
-  zenity --info --text="將manifest.json的路徑餵入即可"
+  yad --info --text="將manifest.json的路徑餵入即可"
 }
 
 case "$1" in
